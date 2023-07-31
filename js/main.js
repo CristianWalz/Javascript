ce612228a22b9cfa67f21e612f856ff5b82f216a
Cristian Walz <cristiannwalz@gmail.com>
2023-07-31 16:14:47 -0300

    Proyecto coder linea 339

--- js/main.js
+++ js/main.js
@@ -108,17 +108,279 @@ var otroNumero =  "20";
 var resultado = unNumero + otroNumero;
 console.log(resultado); */
 
-/* let nombreUno = "Cristian "
-let apellidoUno = "Walz"
-let edad = 28
-let sumaEntre = nombreUno + apellidoUno
 
-prompt("Tu nombre es : ", nombreUno)
-prompt ("Tu apellido es :", apellidoUno)
-prompt("Tu edad es : ", edad) */
+ /* let edad = prompt("Ingrese su edad");
 
-/* let textUno = "Cristian "
-let textDos = "Walz"
-let sumaEntre = textUno + textDos
-prompt("Tu nombre y edad es: ", sumaEntre)
+if (edad >= 18) {
+    alert("Sos mayor de edad. Podés ingresar");
+}else {
+    alert("Sos menor de edad no podes ingresar");
+} 
+ */
+
+/* let edad = 20;
+
+if (edad >= 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+ */
+/* let hora = prompt("Ingrese la hora")
+
+if (hora >= 6 && hora < 12) {
+    alert("Buenos dias");
+} else if (hora >= 12 && hora < 20) {
+    alert("Buenas tardes");
+} else {
+    alert("Buenas noches");
+} */
+
+/* let usuarioUno = "Cristian";
+let usuarioDos = "Walz";
+let nombreDeUsuario = prompt("Ingrese su nombre de usuario")
+
+if (nombreDeUsuario === usuarioUno || nombreDeUsuario === usuarioDos) {
+    alert("Bienvenido " + nombreDeUsuario);
+}else {
+    alert("Nombre de usuario incorrecto")
+} */
+
+// Utilizando un bucle for, imprime los números del 1 al 10 en la consola.
+
+/* for (let i = 0; i <= 10; i = i + 1) {
+    console.log(i)
+} */
+//Utilizando un bucle while, imprime los números pares del 2 al 20 en la consola.
+
+//for
+//for (let x = 0; x < 10; x++) {
+//  console.log("Ciclo número " + x);
+//}
+
+
+// for anidado
+// for (let x = 0; x < 10; x++) {
+  //   console.log("Ciclo número " + x);
+
+    // for (let y = 0; y < 5; y++) {
+      //   console.log("Ciclo secundario " + y);
+    //}
+// }
+
+
+// while
+// let x = 5;
+// while (x < 5) {
+//     console.log("Ciclo while número " + x);
+//     x = x + 1;
+// }
+
+
+// do...while
+// let z = 5;
+// do {
+//     console.log("Ciclo do...while número " + z);
+//     z++;
+// } while (z < 5);
+
+
+// switch
+// let diaDeLaSemana = Number(prompt("Ingrese día de la semana con números"));
+// switch (diaDeLaSemana) {
+//     case 1:
+//         alert("Lunes");
+//         break;
+//     case 2:
+//         alert("Martes");
+//         break;
+//     case 3:
+//         alert("Miércoles");
+//         break;
+//     case 4:
+//         alert("Jueves");
+//         break;
+//     case 5:
+//         alert("Viernes");
+//         break;
+//     case 6:
+//         alert("Sábado");
+//         break;
+//     case 7:
+//         alert("Domingo");
+//         break;
+//     default:
+//         alert("Ese día no existe");
+//         break;
+// }
+
+//Imprimir los números del 1 al 10 en la consola.
+
+//let x = 0
+
+//for (let x = 0; x <= 10; x = x + 1) {
+//    console.log("Numero " + x)
+//}
+
+// Imprimir los números pares del 2 al 20 en la consola.
+
+//let x = 0
+
+//for (let x = 0; x <= 20; x = x + 2) {
+//    console.log("x ahora vale " + x)
+//}
+
+//Calcular la suma de los primeros 50 números naturales (1 + 2 + 3 + ... + 50) y mostrar el resultado en la consola.
+
+//let suma = 0
+
+//for (x = 1; x <= 50; x = x + 1) {
+//    suma = suma + x
+//}
+// console.log("La suma de todos los numeros es " + suma)
+
+// Crear un programa que solicite al usuario un número y luego imprima la tabla de multiplicar de ese número del 1 al 10.
+/* let numero = prompt("Ingrese un numero del 1 al 10");
+numero = parseInt(numero); // Convertir el valor ingresado a un número entero
+
+// Verificar si el valor ingresado es un número válido
+if (isNaN(numero) || numero < 1 || numero > 10) {
+  console.log("Ha ingresado un valor no válido. Por favor, ingrese un número del 1 al 10.");
+} else {
+  // Si el valor es válido, imprimir la tabla de multiplicar
+  console.log("Tabla de multiplicar del " + numero + ":");
+  for (let x = 1; x <= 10; x++) {
+    let resultado = numero * x;
+    console.log(numero + " x " + x + " = " + resultado);
+  }
+} */
+
+//Crear un programa que imprima la siguiente secuencia de números en la consola: 1, 4, 7, 10, 13, ..., hasta llegar a 100. Utilizar un bucle for.
+
+/* let x = 1
+
+for (x = 1; x <= 100; x = x + 3) {
+    console.log("x vale " + x)
+} */
+
+//Imprimir los números del 1 al 10 en la consola utilizando un bucle while.
+
+/* let x = 1
+
+while (x <= 10) {
+    console.log("Numeros del " + x)
+    x = x + 1
+} */
+
+//Imprimir los números impares del 1 al 20 en la consola utilizando un bucle while.
+
+/* let x = 1
+
+while(x <= 20) {
+    console.log("ahora 'x' vale " + x)
+    x = x + 2
+} */
+
+//Calcular la suma de los primeros 100 números naturales (1 + 2 + 3 + ... + 100) y mostrar el resultado en la consola utilizando un bucle while.
+
+/* let x = 1; // Comenzamos con 1 para incluir el número 1 en la suma
+let suma = 0;
+
+while (x <= 100) {
+    console.log("Ahora 'x' vale " + x);
+    suma = suma + x; // Agregamos el valor de 'x' a la suma
+    x = x + 1;
+}
+
+console.log("La suma final es: " + suma);
+ */
+
+//Crear un programa que imprima la siguiente secuencia de números en la consola: 100, 90, 80, 70, ..., hasta llegar a 0. Utilizar un bucle while.
+
+/* let x = 100;
+
+while (x >= 0) {
+    console.log("Secuencia de numeros " + x);
+    x = x - 10; // Restar 10 para obtener la secuencia deseada
+}
+ */
+
+//function sumar(num1, num2) {
+    //let resultado = num1 + num2;
+    //return resultado;
+  //}
+  
+  //let resultadoSuma = sumar(5, 3); // Llamamos a la función sumar con los valores 5 y 3
+  //console.log(resultadoSuma); // El resultado será 8
+
+  // Función anónima tradicional
+/* let saludar = function(nombre) {
+    return "Hola, " + nombre + "!";
+  };
+  
+  // Función flecha
+  let sumar = (num1, num2) => {
+    return num1 + num2;
+  };  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Algoritmo con un condicional 
+
+/* let numero = prompt("Ingrese su edad")
+numero = parseInt(numero)
+
+let ninio = 17
+let adulto = 18
+let anciano = 60
+
+
+if (numero <= ninio) {
+    console.log("Usted es un niño")
+}else if (numero >= adulto && numero < 60) {
+    console.log("Usted es un adulto")
+}else
+    console.log("Usted es un anciano") */
+
+
+//Crear un algoritmo utilizando un ciclo.
+
+/* let x = 0
+
+while (x < 4 ) {
+    console.log("Ahora x vale " + x)
+    x = x + 1
+    alert("x vale " + x )
+} */
+
+// Armar un simulador interactivo, la estructura final de tu proyecto integrador
+
+
+/* let hincha = "river"
+let hinchaDos = "boca"
+let hinchaTres = "independiente"
+
+let nombreHincha = prompt("Cual fue el último campeòn nacional argentino?")
+nombreHincha = nombreHincha.toLowerCase(); // Convertir la entrada del usuario a minúsculas
+
+if (nombreHincha === hincha.toLocaleLowerCase()) { //-- Al final de .toLocaleLowerCase AGREGAR () para invocar la función
+    alert("Tu respuesta es correcta")
+}else if (nombreHincha === hinchaTres.toLocaleLowerCase()) {
+    alert("La respuesta no es Independiente")
+}else if (nombreHincha === hinchaDos.toLocaleLowerCase()) {
+    alert("La respuesta no es Boca")
+}else {
+    alert("Respuesta incorrecta")
+}  
  */
